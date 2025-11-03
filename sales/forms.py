2680236a50a78fb093455197a6e19a5a3949ef33
KaykySiq <josekayky@hotmail.com>
2025-11-03 11:30:43 -0300

    feat: cria os forms.

---
+++ sales/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Sale, SaleItem
+
+class SaleForm(forms.ModelForm):
+    class Meta:
+        model = Sale
+        fields = ['subtotal']  
+
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'input'})
