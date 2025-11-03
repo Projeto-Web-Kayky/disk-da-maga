6a7cbb28a86ebd6aa967a5ad1fa019f64f424326
KaykySiq <josekayky@hotmail.com>
2025-11-03 11:30:57 -0300

    feat: cria as models de venda.

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class Sale(models.Model):
+    sale_date = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product, through='SaleItem')
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Venda {self.id}"
+    
+class SaleItem(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
