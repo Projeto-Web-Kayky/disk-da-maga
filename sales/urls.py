7cfdf4063ecd20b21bd165ee205ea3d5aad52443
KaykySiq <josekayky@hotmail.com>
2025-11-10 16:19:42 -0300

    feat: adiciona o modal de qrcode.

--- sales/urls.py
+++ sales/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:sale_id>/', views.sale_detail, name='sale_detail'),
     path('<int:sale_id>/add-item/', views.add_item, name='add_item'),
     path('<int:sale_id>/pay/', views.pay_sale, name='pay_sale'),
+    path('<int:sale_id>/pix-qr/', views.pix_qr, name='sale_pix_qr'),
     path('<int:sale_id>/remove-item/<int:item_id>/', views.remove_item, name='remove_item'),
     path('<int:sale_id>/search-products/', views.search_products, name='search_products'),
 ]
