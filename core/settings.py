ac1bf61ee89250628911b3dc1bf61f25319b3bd6
KaykySiq <josekayky@hotmail.com>
2025-11-25 12:35:02 -0300

    WIP.

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,10 +66,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://bancomaga_user:1i19zINGaQNdVjuJ3LPQGV3uZLofnmmd@dpg-d4ir444hg0os73a558ag-a/bancomaga'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
