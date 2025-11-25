810ad2df80561f9a149ed8dddb3564e3af6b6301
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2025-11-25 14:52:48 -0300

    Deploy inicial

--- core/settings.py
+++ core/settings.py
@@ -12,13 +12,7 @@ SECRET_KEY = (
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "0.0.0.0",
-    "disk-da-maga.onrender.com",  # substitua se for outro nome
-]
-
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
