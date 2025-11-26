35a3715773c659c6568c02845fc53ea280676161
KaykySiq <josekayky@hotmail.com>
2025-11-26 13:23:24 -0300

    feat: Cria o modal de deletar cliente.

--- clients/urls.py
+++ clients/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import client_list, client_detail, client_delete, client_edit, client_clear_debts
+from .views import client_delete, client_list, client_detail, client_delete, client_edit, client_clear_debts
 
 urlpatterns = [
     path('', client_list, name='client_list'),
     path('<int:client_id>/', client_detail, name='client_detail'),
+    path('<int:client_id>/delete-modal/', client_delete, name='client_delete_modal'),
     path('<int:client_id>/delete/', client_delete, name='client_delete'),
     path('<int:client_id>/edit/', client_edit, name='client_edit'),
     path('<int:client_id>/clear-debts/', client_clear_debts, name='client_clear_debts'),
